e1c0202b158aba1dd326d43e9601de2dc70755f2
Tyler Brown <tylerjbrown45@gmail.com>
2020-02-02 12:20:53 -0600

    k

--- pod/__init__.py
+++ pod/__init__.py
@@ -1,22 +1,22 @@
 from flask import Flask
-from flask_wtf.csrf import CSRFProtect
-from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
+from flask_sqlalchemy import SQLAlchemy
+from flask_wtf.csrf import CSRFProtect
 
 db = SQLAlchemy()
 login_manager = LoginManager()
-login_manager.login_view = 'users.login'
-login_manager.login_message_category = 'info'
+login_manager.login_view = "users.login"
+login_manager.login_message_category = "info"
 
 
 def create_app():
     app = Flask(__name__)
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    app.config['SECRET_KEY'] = 'RANDOMSTRING'
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///site.db"
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    app.config["SECRET_KEY"] = "RANDOMSTRING"
     app.jinja_env.trim_blocks = True
     app.jinja_env.lstrip_blocks = True
-    app.jinja_env.add_extension('jinja2.ext.do')
+    app.jinja_env.add_extension("jinja2.ext.do")
 
     db.init_app(app)
     login_manager.init_app(app)
@@ -24,6 +24,7 @@ def create_app():
 
     from pod.users.routes import users
     from pod.picks.routes import picks
+
     app.register_blueprint(users)
     app.register_blueprint(picks)
 
